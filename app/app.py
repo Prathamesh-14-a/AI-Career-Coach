2b05a05acfabc2d701c295f3ff856b93b643659f
Prathamesh-14-a <prathmeshambulge56@gmail.com>
2026-06-07 21:33:32 +0530

    Authentication Setup Completed

---
+++ app/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Career Coach",
+    layout="wide"
+)
+
+st.title("AI Career Coach")
+
+st.write(
+    "Use the sidebar to Signup or Login."
+)
